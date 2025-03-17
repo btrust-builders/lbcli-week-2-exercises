d12a2f03ae884a6fe00705f1905e18ade52adea8
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2025-03-17 20:05:25 +0300

    fix: add solutions

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+P2SH_ADDRESS=$(bitcoin-cli -regtest getnewaddress "segwitadd" "p2sh-segwit")
+bitcoin-cli -regtest generatetoaddress 101 "$P2SH_ADDRESS" > /dev/null
+echo "$P2SH_ADDRESS"
